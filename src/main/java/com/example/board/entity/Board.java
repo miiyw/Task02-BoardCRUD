7f27b56083826b7a6ec24eb4e85c91b921a135d7
이연우 <yeunwoo217@gmail.com>
2025-07-29 14:24:58 +0900

    feat: 회원 및 게시글 CRUD 기능 구현 (Spring Data JPA)

--- src/main/java/com/example/board/entity/Board.java
+++ src/main/java/com/example/board/entity/Board.java
@@ -1,7 +1,9 @@
 package com.example.board.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "board")
 public class Board extends BaseEntity {
@@ -19,4 +21,15 @@ public class Board extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    public Board() {
+    }
+
+    public Board(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
